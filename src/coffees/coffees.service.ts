aed0913d369286aa392ad998f99b183e93e12f91
CarlosIvan1997 <carlosipm69@gmail.com>
2021-04-01 21:05:49 -0500

    feat(coffees): add dto transform

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -16,8 +16,8 @@ export class CoffeesService {
     return this.coffees;
   }
 
-  findOne(id: string) {
-    const coffee = this.coffees.find((item) => item.id === +id);
+  findOne(id: number) {
+    const coffee = this.coffees.find((item) => item.id === id);
     if (!coffee) {
       // throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
       throw new NotFoundException(`Coffee #${id} not found`);
@@ -29,15 +29,15 @@ export class CoffeesService {
     this.coffees.push(createCoffeeDto);
   }
 
-  update(id: string, updateCoffeeDto: any) {
+  update(id: number, updateCoffeeDto: any) {
     const existingCoffee = this.findOne(id);
     if (existingCoffee) {
       // update the existing entity
     }
   }
 
-  remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+  remove(id: number) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === id);
     if (coffeeIndex >= 0) {
       this.coffees.splice(coffeeIndex, 1);
     }
